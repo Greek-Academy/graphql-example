2c07995408a27052a2ca68cc38131074a8597447
Masashi Katsumata <wf9a5m75@gmail.com>
2024-10-03 01:44:07 -0700

    prepare for tutorial

--- app/src/schema.ts
+++ app/src/schema.ts
@@ -17,31 +17,8 @@ export const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => { return "Hello World!!"; },
-      },
-      image: {
-        type: ImageType,
-        args: {
-          id: {
-            description: 'id of the image',
-            type: new GraphQLNonNull(GraphQLInt),
-          },
-        },
-        resolve: (_source, { id }) => getImageById(id),
-      },
-      images: {
-        type: new GraphQLList(ImageType),
-        args: {
-          category: {
-            description: 'image category',
-            type: GraphQLString,
-          }
-        },
-        resolve: (_source, { category }) => getImages(category),
-      }
-    },
+      // ここにフィールドを追加
+    }
   }),
 });
 
